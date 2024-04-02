b200bfafffa46987c0c0bf56c52f18a1a1d660d9
Dom Webber <dom.webber@hotmail.com>
2024-04-02 09:25:41 +0100

    adjust cli output formatting

--- extract.sh
+++ extract.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 image=$1
+grep_for=$2
 extract_to=images/$image
 
-tar_file=image.tar
-echo "Extracting $image to $extract_to"
+echo "Extracting $image to $extract_to\n"
 
 mkdir -p $extract_to
 docker save $image | tar -C $extract_to -xvf -
 
-grep_for=$2
-
-echo "Finding $grep_for in $extract_to..."
+echo "\nFinding $grep_for in $extract_to\n"
 echo "Found $grep_for in:"
 
 grep -r $grep_for $extract_to
